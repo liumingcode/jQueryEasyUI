bd7463a1e0741100a314d22f6f113f0c049b74f7
刘明明 <liuming_music@qq.com>
2015-05-10 16:29:50 +0800

    add folder 03

---
+++ 03/js/demo.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+	$("#resizbale").resizable({
+		minWidth : 100,
+		minHeight : 100,
+		maxWidth : 500,
+		maxHeight : 500,
+		edge : 20,
+		disabled : false,
+		handles : 'all',
+	})
+
+})
